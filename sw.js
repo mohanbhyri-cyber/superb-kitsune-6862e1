b47e4af2b4ef3ded0778c3d25750c7050a49d87f
mohanbhyri-cyber <mohanbhyri@gmail.com>
2026-09-15 22:27:03 +0530

    Refresh PWA cache after removing preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stride-v7';
+const CACHE='stride-v8';
 const ASSETS=['./','./index.html','./style.css','./app.js','./market.js','./price-action.js','./signal-alerts.js','./pro-scalper.js','./momentum.js','./icon.svg','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('stride-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
